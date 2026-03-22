c0ebd11651da36f31c12728e8d7dc66e4d106188
raakeshguptaa <rakesh.works20@gmail.com>
2026-03-22 20:56:28 +0530

    some fix's

--- app/rate_limiter/limiter.py
+++ app/rate_limiter/limiter.py
@@ -5,20 +5,36 @@ import time
 
 r = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
+rate_limit = 5
+window_size = 10
 
 async def add(request: Request, call_next):
 
     now = time.time()
+    user_key = str(DECODED_USER_ID)
 
     request_id = getattr(request.state, "request_id", "unknown")
 
-    r.zadd(str(DECODED_USER_ID), {request_id: now})
+    #add current request 
+    r.zadd(user_key, {request_id: now})
 
-    sortedset = r.zrangebyscore(str(DECODED_USER_ID), 0, now, withscores=True)
+    #remove old request (outside window)
+    r.zremrangebyscore(user_key,0,now - window_size)
 
-    print("User ID:", DECODED_USER_ID)
-    print("Redis data:", sortedset)
+    request_count = r.zcard(user_key)
 
+    print("USER ID:",user_key)
+    print("Request Count:", request_count)
+
+    #check limit
+
+    if request_count > rate_limit:
+        print("overflow")
+
+        
     response = await call_next(request)
 
+
     return response
+
+
